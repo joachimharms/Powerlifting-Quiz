67cc06538b12d2039d01b855c801ad1157bc2848
joachimharms <joachimharms77@gmail.com>
2017-09-10 00:45:39 +0200

    Implement methods for every question - done

--- app/src/main/java/com/example/android/powerliftingquiz/MainActivity.java
+++ app/src/main/java/com/example/android/powerliftingquiz/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Checkable;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
@@ -19,6 +22,49 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
+
+    public void checkQuestOne()
+    {
+        CheckBox checkboxSquat = (CheckBox) findViewById(R.id.checkbox_squat);
+        CheckBox checkBoxBench = (CheckBox) findViewById(R.id.checkbox_bench_press);
+        CheckBox checkBoxDeadlift = (CheckBox) findViewById(R.id.checkbox_deadlift);
+        CheckBox checkBoxOverhead = (CheckBox) findViewById(R.id.checkbox_overhead_press);
+        if((checkboxSquat.isChecked()) && (checkBoxBench.isChecked()) && (checkBoxDeadlift.isChecked()) && (!checkBoxOverhead.isChecked()))
+        {
+                score++;
+        }
+    }
+
+    public void checkQuestTwo()
+    {
+        RadioButton radioButtonRightAns = (RadioButton) findViewById(R.id.radio_button_right_bottom);
+        if (radioButtonRightAns.isChecked())
+        {
+            score++;
+        }
+    }
+
+    public void checkQuestThree()
+    {
+        RadioButton radioButtonRightAns = (RadioButton) findViewById(R.id.radiobutton_no);
+        if (radioButtonRightAns.isChecked())
+        {
+            score++;
+        }
+    }
+
+    public void checkQuestFour()
+    {
+        CheckBox checkBoxSchumacher = (CheckBox) findViewById(R.id.checkbox_schumacher);
+        CheckBox checkBoxKarkowski = (CheckBox) findViewById(R.id.checkbox_karkowski);
+        CheckBox checkBoxCoan = (CheckBox) findViewById(R.id.checkbox_coan);
+        CheckBox checkBoxMalanichev = (CheckBox) findViewById(R.id.checkbox_malanichev);
+        if((checkBoxKarkowski.isChecked()) && (checkBoxCoan.isChecked()) && (checkBoxMalanichev.isChecked()) && (!checkBoxSchumacher.isChecked()))
+        {
+            score++;
+        }
+
+    }
     public void checkQuestFive()
     {
         EditText editText = (EditText) findViewById(R.id.edit_text_view);
