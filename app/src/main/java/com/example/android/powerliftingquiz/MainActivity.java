a3a8ff2acdebc4f2a60735ac0771759fd4ad9533
joachimharms <joachimharms77@gmail.com>
2017-09-09 16:35:08 +0200

    Implement method to check for answer of question five - done - needs unit test, debugging and logging

--- app/src/main/java/com/example/android/powerliftingquiz/MainActivity.java
+++ app/src/main/java/com/example/android/powerliftingquiz/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.android.powerliftingquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity
 {
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -12,4 +18,21 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void checkQuestFive()
+    {
+        EditText editText = (EditText) findViewById(R.id.edit_text_view);
+        String ans = editText.getText().toString();
+        if(ans.contains("raw") || ans.contains("Raw"))
+        {
+            score ++;
+            Log.v("MainActivity", "Value: " + score);
+        }
+
+    }
+
+
+
+
+
 }
